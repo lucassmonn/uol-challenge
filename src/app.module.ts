0270269abddea173904830e1329e3a26d9cd5f02
Lucas Monteiro <lucassmonn@gmail.com>
2023-04-25 21:10:29 -0300

    chore: add envs validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { envsValidator } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envsValidator,
+      validationOptions: {
+        abortEarly: true,
+        whitelist: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/envs-validator.ts
@@ -0,0 +1,6 @@
+import Joi from '@hapi/joi';
+
+export const envsValidator = Joi.object({
+  APP_PORT: Joi.number().default(3000),
+  DATABASE_URL: Joi.string().required(),
+});
