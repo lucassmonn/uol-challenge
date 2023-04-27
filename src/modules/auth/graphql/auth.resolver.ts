773631f293799e2e3afc343c7f82c12469bcea51
Lucas Monteiro <lucassmonn@gmail.com>
2023-04-27 00:37:28 -0300

    feat: create resolver base

--- src/modules/auth/graphql/auth.resolver.ts
+++ src/modules/auth/graphql/auth.resolver.ts
@@ -1,13 +1,16 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { ResolverBase } from '@shared/base/resolver.base';
 import { LoginInput } from '../inputs/login.input';
 import { LoginUseCase } from '../usecases/login.usecase';
 import { LoginObject } from './login.object';
 
 @Resolver()
-export class AuthResolver {
-  constructor(private readonly loginUseCase: LoginUseCase) {}
+export class AuthResolver extends ResolverBase {
+  constructor(private readonly loginUseCase: LoginUseCase) {
+    super();
+  }
   @Mutation(() => LoginObject)
   async login(@Args('input') input: LoginInput): Promise<LoginObject> {
-    return await this.loginUseCase.execute(input);
+    return await this.callUseCase(this.loginUseCase, input);
   }
 }
---
+++ src/shared/base/resolver.base.ts
@@ -0,0 +1,14 @@
+import { UseCaseBase } from './usecase.base';
+
+export class ResolverBase {
+  async callUseCase<Input, Output>(
+    useCase: UseCaseBase<Input, Output>,
+    input: Input,
+  ): Promise<Output | Error | any> {
+    try {
+      return await useCase.execute(input);
+    } catch (error) {
+      return error;
+    }
+  }
+}
